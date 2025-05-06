b41d037b2c70aa7eb481d762cf19686caa140ab0
Dumble009 <daichi19990821@gmail.com>
2025-05-06 14:25:02 +0900

    canvas_grid にグリッドサイズを変更する機能を追加

--- src/common/canvas_grid.rs
+++ src/common/canvas_grid.rs
@@ -29,11 +29,71 @@ impl CanvasGrid {
     }
 
     pub fn get_grid_width(&self) -> usize {
-        INITIAL_GRID_WIDTH
+        if self.get_grid_height() == 0 {
+            return 0;
+        }
+        self.grid[0].len()
     }
 
     pub fn get_grid_height(&self) -> usize {
-        INITIAL_GRID_HEIGHT
+        self.grid.len()
+    }
+
+    pub fn set_grid_width(&mut self, new_w: usize) -> Result<(), String> {
+        if new_w == 0 {
+            return Err(String::from("cannot set grid width 0."));
+        }
+
+        let old_w = self.get_grid_width();
+
+        if new_w == old_w {
+            return Ok(());
+        }
+
+        if new_w > old_w {
+            let diff = new_w - old_w;
+            for row in self.grid.iter_mut() {
+                for _ in 0..diff {
+                    row.push(0);
+                }
+            }
+        } else {
+            let diff = old_w - new_w;
+            for row in self.grid.iter_mut() {
+                for _ in 0..diff {
+                    row.pop();
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    pub fn set_grid_height(&mut self, new_h: usize) -> Result<(), String> {
+        if new_h == 0 {
+            return Err(String::from("cannot set grid height 0."));
+        }
+
+        let old_h = self.get_grid_height();
+
+        if new_h == old_h {
+            return Ok(());
+        }
+
+        if new_h > old_h {
+            let diff = new_h - old_h;
+            let width = self.get_grid_width();
+            for _ in 0..diff {
+                self.grid.push(vec![0; width]);
+            }
+        } else {
+            let diff = old_h - new_h;
+            for _ in 0..diff {
+                self.grid.pop();
+            }
+        }
+
+        Ok(())
     }
 
     fn coordinate_validation(&self, x: usize, y: usize) -> Result<(), String> {
--- src/common/tests/canvas_grid_test.rs
+++ src/common/tests/canvas_grid_test.rs
@@ -40,4 +40,67 @@ mod test {
         let set_res = canvas_grid.set_color(width, height, 1);
         assert!(set_res.is_err());
     }
+
+    #[test]
+    fn canvas_grid_set_width_height_test() {
+        let mut canvas_grid = CanvasGrid::new();
+
+        let new_width = INITIAL_GRID_WIDTH + 1;
+        let new_height = INITIAL_GRID_HEIGHT + 10;
+
+        let res = canvas_grid.set_grid_width(new_width);
+        assert!(res.is_ok());
+        let res = canvas_grid.set_grid_height(new_height);
+        assert!(res.is_ok());
+
+        let width = canvas_grid.get_grid_width();
+        let height = canvas_grid.get_grid_height();
+
+        assert_eq!(width, new_width);
+        assert_eq!(height, new_height);
+
+        // out of range にならずにアクセスできる範囲も正常に広がっているか?
+        let get_res = canvas_grid.get_color(INITIAL_GRID_WIDTH, INITIAL_GRID_HEIGHT);
+        assert!(get_res.is_ok());
+
+        let set_res = canvas_grid.set_color(INITIAL_GRID_WIDTH, INITIAL_GRID_HEIGHT, 1);
+        assert!(set_res.is_ok());
+
+        let res = canvas_grid.set_grid_width(INITIAL_GRID_WIDTH);
+        assert!(res.is_ok());
+        let res = canvas_grid.set_grid_height(INITIAL_GRID_HEIGHT);
+        assert!(res.is_ok());
+
+        let width = canvas_grid.get_grid_width();
+        let height = canvas_grid.get_grid_height();
+
+        assert_eq!(width, INITIAL_GRID_WIDTH);
+        assert_eq!(height, INITIAL_GRID_HEIGHT);
+
+        // 縮小したら out of range になる範囲もそれに合わせて変わっているか?
+        let get_res = canvas_grid.get_color(INITIAL_GRID_WIDTH, INITIAL_GRID_HEIGHT);
+        assert!(get_res.is_err());
+
+        let set_res = canvas_grid.set_color(INITIAL_GRID_WIDTH, INITIAL_GRID_HEIGHT, 1);
+        assert!(set_res.is_err());
+
+        // 現在と同じ値にも設定できる
+        let res = canvas_grid.set_grid_width(INITIAL_GRID_WIDTH);
+        assert!(res.is_ok());
+        let res = canvas_grid.set_grid_height(INITIAL_GRID_HEIGHT);
+        assert!(res.is_ok());
+
+        // 0には設定できない
+        let res = canvas_grid.set_grid_width(0);
+        assert!(res.is_err());
+        let res = canvas_grid.set_grid_height(0);
+        assert!(res.is_err());
+
+        // 0に設定しようとしても値は変わらない
+        let width = canvas_grid.get_grid_width();
+        let height = canvas_grid.get_grid_height();
+
+        assert_eq!(width, INITIAL_GRID_WIDTH);
+        assert_eq!(height, INITIAL_GRID_HEIGHT);
+    }
 }
