12598d6726524667230e2a68278dabae6636204d
Dumble009 <daichi19990821@gmail.com>
2026-08-16 14:25:17 +0900

    ColorSet を追加

--- src/common/color.rs
+++ src/common/color.rs
@@ -21,4 +21,33 @@ impl ODColor {
     }
 }
 
+pub const BRIGHTNESS_RANGE: usize = 10;
+pub struct ColorSet {
+    colors: [ODColor; BRIGHTNESS_RANGE * 2 + 1],
+}
+
+impl ColorSet {
+    pub const fn new(color: ODColor) -> Self {
+        let colors: [ODColor; BRIGHTNESS_RANGE * 2 + 1] = [color; BRIGHTNESS_RANGE * 2 + 1];
+        ColorSet { colors }
+    }
+
+    pub fn get_color(&self, brightness: i32) -> Result<ODColor, String> {
+        if i32::abs(brightness) > BRIGHTNESS_RANGE as i32 {
+            return Err(format!("invalid brightness {brightness}"));
+        }
+
+        Ok(self.colors[BRIGHTNESS_RANGE + brightness as usize])
+    }
+
+    pub fn set_color(&mut self, brightness: i32, color: ODColor) -> Result<(), String> {
+        if i32::abs(brightness) > BRIGHTNESS_RANGE as i32 {
+            return Err(format!("invalid brightness {brightness}"));
+        }
+
+        self.colors[BRIGHTNESS_RANGE + brightness as usize] = color;
+        Ok(())
+    }
+}
+
 include!("tests/color_test.rs");
--- src/common/tests/color_test.rs
+++ src/common/tests/color_test.rs
@@ -17,4 +17,25 @@ mod test {
 
         assert_eq!(color, ODColor::new(1, 2, 3));
     }
+
+    #[test]
+    fn get_set_color_set() {
+        let color = ODColor::new(1, 2, 3);
+        let mut color_set = ColorSet::new(color);
+
+        let color = color_set.get_color(0).unwrap();
+        assert_eq!(color, ODColor::new(1, 2, 3));
+
+        let res = color_set.set_color(1, ODColor::new(2, 4, 6));
+        assert!(res.is_ok());
+
+        let color = color_set.get_color(1).unwrap();
+        assert_eq!(color, ODColor::new(2, 4, 6));
+
+        let res = color_set.get_color(100);
+        assert!(res.is_err());
+
+        let res = color_set.set_color(100, ODColor::new(5, 6, 7));
+        assert!(res.is_err());
+    }
 }
